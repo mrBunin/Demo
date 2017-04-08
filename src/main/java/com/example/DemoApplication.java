34ac624db8e5aac5a9c4ecaf24703a47b6ccf95b
Oleg Shabunin <mr.bunin@gmail.com>
2017-04-08 16:42:33 -0400

    Used different profiles.

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,17 +1,11 @@
 package com.example;
 
-import com.example.setup.FooProperties;
-import com.example.setup.MoreProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
-@PropertySource(value = {"classpath:application.properties", "classpath:more.properties"})
-@EnableConfigurationProperties(FooProperties.class)
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -19,23 +13,7 @@ public class DemoApplication {
     }
 
     @Bean
-    public ParamPublisher publisher() {
-        return new ParamPublisher();
-    }
-
-    @Bean
-    public FooProperties fooProperties() {
-        return new FooProperties();
-    }
-
-    @Bean
-    public MoreProperties moreProperties() {
-        return new MoreProperties();
-    }
-
-    @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
-
 }
---
+++ src/main/java/com/example/profile/TestProfile.java
@@ -0,0 +1,20 @@
+package com.example.profile;
+
+import com.example.WorkEnvironment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by Oleg Shabunin on 4/8/2017.
+ */
+
+@Configuration
+@Profile("test")
+public class TestProfile {
+
+    @Bean
+    public WorkEnvironment environment() {
+        return new WorkEnvironment();
+    }
+}
